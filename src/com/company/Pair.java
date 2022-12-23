6177fcbf7b8e600badab4b59766670633d0515a7
Akash Yadagouda <ayadagouda@onetrust.com>
2022-12-23 12:02:44 +0530

    Activity Selection problem done

---
+++ src/com/company/Pair.java
@@ -0,0 +1,9 @@
+
+import javafx.util.Pair;
+
+public class Pair{
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		pair<Integer, String> p = new pair<Integer, String>(10, "Hello Geeks!");
+	}
+}
