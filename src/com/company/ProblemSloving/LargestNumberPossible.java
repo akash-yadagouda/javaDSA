f0bd421366eb989d5b3a47a6dadf036c18ff361e
akash-yadagouda <akashyadagoud@gmail.com>
2022-12-25 13:25:00 +0530

    LargestNumberPossible

--- src/com/company/ProblemSloving/LargestNumberPossible.java
+++ src/com/company/ProblemSloving/LargestNumberPossible.java
@@ -1,24 +1,56 @@
 
 import java.util.*;
 
-public class LargestNumberPossible{
+public class LargestNumberPossible {
+
+
+	public static String findLargest(int N, int S) {
+		// code here
+		int[] arr = new int[N];
+		int i = 0;
+
+
+		if (S > N * 9) {
+			return "-1";
+		}
+
+		if (N == 1 && S == 0) {
+			return "0";
+		}
+
+		if (N > 1 && S == 0) {
+			return "-1";
+		}
+
+		while (S > 0) {
+			if (S >= 9) {
+				arr[i] = 9;
+				S = S - 9;
+				i++;
+			} else {
+				arr[i] = S;
+				S = S - arr[i];
+				i++;
+			}
+		}
+
+		System.out.println(Arrays.toString(arr));
+
+		StringBuilder st = new StringBuilder();
+
+		for (int k = 0; k < N; k++) {
+			st.append(String.valueOf(arr[k]));
+		}
+
+		return st.toString();
+	}
 
 	public static void main(String[] args) {
 		ReadFromFile.readFromFile();
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt(); // digits
 		int S = scan.nextInt(); // sum
-
-		String nine = "9";
-
-		int[] arrayofnine = new int[N];
-
-		Arrays.fill(arrayofnine, 9);
-
-		System.out.println(Arrays.toString(arrayofnine));
-
-		Arrays.sort(arrayofnine
-
+		System.out.println("num :" + findLargest(N, S));
 
 	}
 
